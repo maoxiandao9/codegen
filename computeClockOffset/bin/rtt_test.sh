93962b6b378d00968720d15a416e729feb696c94
amumu <136903859@qq.com>
2024-12-03 14:28:11 +0800

    fix bug

--- computeClockOffset/bin/rtt_test.sh
+++ computeClockOffset/bin/rtt_test.sh
@@ -5,12 +5,12 @@ if ! lsmod | grep -q "^side_ch"; then
     echo "side_ch.ko 未加载，尝试加载模块..."
     # 如果需要指定模块路径，可以用 insmod
     insmod /root/sdr/side_ch.ko num_eq_init=0
-    # 如果模块已安装在标准模块路径下，使用 modprobe
-    modprobe side_ch
-    if [ $? -ne 0 ]; then
-        echo "加载 side_ch.ko 失败，请检查路径或模块依赖。"
-        exit 1
-    fi
+    # # 如果模块已安装在标准模块路径下，使用 modprobe
+    # modprobe side_ch
+    # if [ $? -ne 0 ]; then
+    #     echo "加载 side_ch.ko 失败，请检查路径或模块依赖。"
+    #     exit 1
+    # fi
     echo "side_ch.ko 已成功加载。"
 else
     echo "side_ch.ko 已加载。"
